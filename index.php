ad4f6608b542c87cc38f95819dfaae8603634972
einnabno <70646362+einnabno@users.noreply.github.com>
2020-10-02 22:21:16 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<script src="https://kit.fontawesome.com/9805e53244.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div class="header">
+			<div class="title">YANNIE'S PIZZA</div>
+			<div class="mushroom"></div>
+				<div class="mushroom1"></div>
+				<div class="mushroom2"></div>
+				<div class="mushroom3"></div>
+			<div class="leaves"></div>
+			    <div class="leaf1"></div>
+				<div class="leaf2"></div>
+				<div class="leaf3"></div>
+				<div class="leaf4"></div>
+				<div class="leaf5"></div>
+		</div>
+			<div class="pizza"></div>
+			<div class="salami"></div>
+			<div class="salami1"></div>
+			<div class="salami2"></div>
+			<div class="salami3"></div>
+			<div class="salami4"></div>
+			<div class="salami5"></div>
+			<div class="salami6"></div>
+			<div class="salami7"></div>
+			<div class="salami8"></div>
+			<div class="salami9"></div>
+			<div class="pepperoni"></div>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+				<div class="bubble"><br>A</div>
+								<div class="bubble1"><br>B</div>
+												<div class="bubble2"><br>O</div>
+																<div class="bubble3"><br>U</div>
+																				<div class="bubble4"><br>T</div>
+																							<div class="bubble5"><br>U</div>
+																										<div class="bubble6"><br>S</div>
+			<div class="right"><br><br><br><br>From humble beginnings as a single pizza restaurant in 1960, Yannie’s Pizza today has become the recognized world leader in pizza delivery. 
+			We have been dedicated to making and delivering great pizza with high quality ingredients from the start. In addition, 
+			we are constantly updating and improving our pizza menu to satisfy the growing needs of our customers.
+			<br>
+			<br>Yannie’s delivered over 400 million pizzas last year. That’s just over one pizza for everyone in the Philippines. 
+			Our delivery people drive more than 10 million miles every week to bring you the best pizza we can make. 
+			We operate over 11,000 pizza places in more than 80 countries dedicated to providing great tasting pizza for carryout or delivered to your door.</div>
+			<div class="container">
+			<div class="p">
+				<span class="circle"></span>
+				<span class="drip"></span>
+			</div>
+			</div>				
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<div class="circ"><br><center>Contact Us @ 495 7786<center></div>
+			<div  class="box"><br><center>Surround YOURSELF with PIZZA, not with NEGATIVITY</div>
+			<div class="box1"><br><center>Choose your Fighter!
+			<br>Pepperoni, Hawaiian, Cheese Overload
+			<br> and Meaty Overload<center></div>
+			<div class="footer"><center>
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-instagram"></a>
+<a href="#" class="fa fa-google"></a>
+			<br>Login | Disclaimer | Privacy Policy | Sitemap</div></center>
+	</body>
+</html>
